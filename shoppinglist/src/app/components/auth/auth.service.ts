99af4dc8f051d590eba26bf7b3965de358e77681
John Garner <jdextergarneriv@gmail.com>
2021-03-16 17:02:52 -0400

    error handling logic moved to service via pipe to make component leaner

--- shoppinglist/src/app/components/auth/auth.service.ts
+++ shoppinglist/src/app/components/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { EnvService } from 'src/app/core/services/envservice/env.service';
 
 interface AuthResponseData {
@@ -16,10 +18,24 @@ export class AuthService {
   constructor(private http: HttpClient, private env: EnvService) {}
 
   signUp(email: string, password: string) {
-    return this.http.post<AuthResponseData>(this.env.signUpUrl, {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    });
+    return this.http
+      .post<AuthResponseData>(this.env.signUpUrl, {
+        email: email,
+        password: password,
+        returnSecureToken: true,
+      })
+      .pipe(
+        catchError((errorRes) => {
+          let errorMessage = 'An unknown error occured!';
+          if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+          }
+          switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage = 'This email exists already, please login.';
+          }
+          return throwError(errorMessage);
+        })
+      );
   }
 }
