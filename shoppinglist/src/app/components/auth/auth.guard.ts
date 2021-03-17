1d0cae134365faba3e910716c7e0a28eb99f1a8c
John Garner <jdextergarneriv@gmail.com>
2021-03-17 14:56:23 -0400

    create UrlTree to redirect to /auth if not isAuth

--- shoppinglist/src/app/components/auth/auth.guard.ts
+++ shoppinglist/src/app/components/auth/auth.guard.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  Router,
   RouterStateSnapshot,
+  UrlTree,
 } from '@angular/router';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -10,16 +12,20 @@ import { AuthService } from './auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     router: RouterStateSnapshot
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  ): boolean | Promise<boolean> | Observable<boolean | UrlTree> {
     return this.authService.user.pipe(
       map((user) => {
         // !! returns true boolean representation
-        return !!user;
+        const isAuth = !!user;
+        if (isAuth) {
+          return true;
+        }
+        return this.router.createUrlTree(['/auth']);
       })
     );
   }
